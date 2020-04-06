e82a48abac79f100f77952d868b720240d3d5d00
Mujahid <mujahid.islam@cloudtek.biz>
2020-04-06 17:51:48 -0400

    added navigation using react-router-dom withRouter High Order Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import {Switch ,Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.compnent';
+
+const Hats = () => (
+
+  <div>
+    <h1>Hats</h1>
+  </div>
+)
+
+const Mens = () =>(
+
+  <div>
+    <h1>Mens</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={Hats} />
+        <Route exact path='/mens' component={Mens} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-const MenuItem = ({ title, imageurl, size }) => (
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
 
-    <div className={`${size} menu-item`} >
+    <div 
+        className={`${size} menu-item`}
+        onClick= {() => history.push(`${match.url}${linkUrl}`)} 
+    >
         <div 
             className="background-image" 
             style={{ 
-                backgroundImage: `url(${imageurl})` 
+                backgroundImage: `url(${imageUrl})` 
             }} 
         />
         <div className="content">
@@ -17,4 +21,4 @@ const MenuItem = ({ title, imageurl, size }) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
