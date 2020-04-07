81758285ba59801b27d677a8587e910e4292b385
Mujahid <mujahid.islam@cloudtek.biz>
2020-04-07 12:09:34 -0400

    created a shop page and created components for displaying collections

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
+import Sections_Data from './sections.data';
 
 class Directory extends React.Component {
 
@@ -9,40 +10,7 @@ class Directory extends React.Component {
         super();
 
         this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1,
-                    linkUrl: 'hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2,
-                    linkUrl: ''
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3,
-                    linkUrl: ''
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: ''
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: 'mens'
-                }
-            ]
+            sections: Sections_Data
         }
     }
 
@@ -57,7 +25,6 @@ class Directory extends React.Component {
             </div>
         );
     }
-    debugger
 }
 
 export default Directory;
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Shop_Data from './shops.data';
+import Collections from '../../components/collections/collections.component'
+
+class ShopPage extends React.Component {
+
+    constructor(props){
+
+        super(props);
+
+        this.state = {
+            collections: Shop_Data
+        } 
+    }
+
+    render(){
+        const {collections} = this.state;
+        return(
+
+            <div className="shop-page">
+                {
+                    collections.map(({id, ...otherCollectionProps}) => (
+                        <Collections key={id} {...otherCollectionProps} />
+                    ))
+                }
+                
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
