efc9261b29c65e7b2cca7437587d91ff54d74776
Mujahid <mujahid.islam@cloudtek.biz>
2020-04-27 14:04:47 -0400

    added signup with email and password, used firestore to save and retrieve data from firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,13 +13,43 @@ const config = {
     measurementId: "G-10YTXFB552"
 };
 
+export const createUserDoc = async (userAuth, additionalData) => {
+
+    if (!userAuth) {
+        return;
+    }
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('Error encountered while creating user', error.message);
+        }
+    }
+
+    return userRef;
+
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({promt:'select_account'});
+provider.setCustomParameters({ promt: 'select_account' });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
