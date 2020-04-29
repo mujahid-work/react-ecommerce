4a6a80d9442adaa158614ad2c147c359a8e0c012
Mujahid <mujahid.islam@cloudtek.biz>
2020-04-29 09:19:13 -0400

    added redux, redux-logger and react-redux, setting up redux and implemented redux for setting current user state

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,9 +3,10 @@ import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser }) => (
 
     <div className="header">
         <Link className="logo-container" to='/'>
@@ -21,12 +22,16 @@ const Header = ({currentUser}) => (
 
             {
                 currentUser ?
-                <div className="option" onClick={() => auth.signOut()}>Sign Out</div>
-                :
-                <Link className="option" to="/sign-in">Sign In</Link>
+                    <div className="option" onClick={() => auth.signOut()}>Sign Out</div>
+                    :
+                    <Link className="option" to="/sign-in">Sign In</Link>
             }
         </div>
     </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
